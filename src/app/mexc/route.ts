35a7d1af25d710bd0de665b8e782f2742781364c
George Yin Xu <georgeyinxu@gmail.com>
2023-08-30 11:21:27 +0800

    set up the route for testing of connection and getting account balance

--- src/app/mexc/route.ts
+++ src/app/mexc/route.ts
@@ -1,11 +1,6 @@
 import { NextResponse } from "next/server";
 import { NextApiRequest, NextApiResponse } from "next";
 
-import Spot from "./spot";
-const client = new Spot(process.env.MEXC_API_KEY, process.env.MEXC_API_SECRET, {
-  baseURL: process.env.MEXC_API_URL,
-});
-
 export async function GET(req: NextApiRequest, res: NextApiResponse) {
   try {
     return NextResponse.json({
